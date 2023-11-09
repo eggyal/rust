988df3265ce1c459370105ece572db0673554a41
Alan Egerton <eggyal@gmail.com>
2023-11-09 07:00:57 +0000

    Skip guaranteed trivial fields in traversables
    Eliminates the need for, and usages of, the dangerous `type_foldable`
    and `type_visitable` derive macro attributes by which fields could be
    explicitly skipped from derived traversals.

--- compiler/rustc_macros/src/traversable.rs
+++ compiler/rustc_macros/src/traversable.rs
@@ -1,107 +1,226 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Ident, Span, TokenStream};
 use quote::{quote, ToTokens};
-use syn::parse_quote;
+use smallvec::SmallVec;
+use std::collections::{HashSet, VecDeque};
+use syn::{
+    parse_quote,
+    visit::{self, Visit},
+    Field, Generics, Lifetime,
+};
+
+use Type::*;
+
+#[cfg(test)]
+mod tests;
+
+/// Generate a type parameter with the given `suffix` that does not conflict with
+/// any of the `existing` generics.
+fn gen_param(suffix: impl ToString, existing: &Generics) -> Ident {
+    let mut suffix = suffix.to_string();
+    while existing.type_params().any(|t| t.ident == suffix) {
+        suffix.insert(0, '_');
+    }
+    Ident::new(&suffix, Span::call_site())
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum Type {
+    /// Describes a type that is not parameterised by the interner, and therefore cannot
+    /// be of any interest to traversers.
+    Trivial,
+
+    /// Describes a type that is parameterised by an internable lifetime (`'tcx`, or any
+    /// other lifetime parameter bounded thereby) but that is otherwise not generic.
+    Internable,
+
+    /// Describes a type that is generic.
+    Generic,
+}
+
+pub struct Interner<'a> {
+    /// Valid lifetimes of the `TyCtxt`; the first is always the `'tcx` lifetime,
+    /// and the remainder are others that are bounded by it. If empty, the derive
+    /// input was not parameterised by `'tcx` and the `TyCtxt` in the generated
+    /// implementation should use a `'tcx` parameter that is unrelated to the input.
+    lifetimes: SmallVec<[&'a Lifetime; 1]>,
+}
+
+impl<'a> Interner<'a> {
+    /// Return the interner for an input with the given `generics`.
+    fn resolve(generics: &'a Generics) -> Self {
+        let mut lifetimes = SmallVec::new();
+        let tcx = parse_quote! { 'tcx };
+
+        let mut queue = VecDeque::from([&tcx]);
+        while let Some(bound) = queue.pop_front() {
+            if !lifetimes.contains(&bound) {
+                for def in generics.lifetimes() {
+                    if def.lifetime == *bound {
+                        lifetimes.push(&def.lifetime);
+                        queue.extend(&def.bounds);
+                        if let Some(where_clause) = &generics.where_clause {
+                            for pred in &where_clause.predicates {
+                                if let syn::WherePredicate::Lifetime(syn::PredicateLifetime {
+                                    lifetime,
+                                    bounds,
+                                    ..
+                                }) = pred && lifetime == bound {
+                                    queue.extend(bounds);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Self { lifetimes }
+    }
+
+    /// We consider a type to be internable if it references either a generic type parameter
+    /// or an internable lifetime.
+    fn type_of<'b>(
+        &self,
+        referenced_ty_params: &[&Ident],
+        fields: impl IntoIterator<Item = &'b Field>,
+    ) -> Type {
+        struct Info<'a> {
+            internable_lifetimes: &'a [&'a Lifetime],
+            can_reference_interner: bool,
+        }
+
+        impl Visit<'_> for Info<'_> {
+            fn visit_lifetime(&mut self, i: &Lifetime) {
+                if self.internable_lifetimes.contains(&i) {
+                    self.can_reference_interner = true;
+                } else {
+                    visit::visit_lifetime(self, i)
+                }
+            }
+        }
+
+        if !referenced_ty_params.is_empty() {
+            Generic
+        } else if !self.lifetimes.is_empty()
+            && fields.into_iter().any(|field| {
+                let mut info =
+                    Info { internable_lifetimes: &self.lifetimes, can_reference_interner: false };
+                info.visit_type(&field.ty);
+                info.can_reference_interner
+            })
+        {
+            Internable
+        } else {
+            Trivial
+        }
+    }
+}
+
+impl ToTokens for Interner<'_> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let default = &parse_quote! { 'tcx };
+        let lt = self.lifetimes.first().unwrap_or(&default);
+        tokens.extend(quote! { ::rustc_middle::ty::TyCtxt<#lt> });
+    }
+}
 
 pub struct Foldable;
 pub struct Visitable;
 
 /// An abstraction over traversable traits.
 pub trait Traversable {
-    /// The trait that this `Traversable` represents.
-    fn traversable() -> TokenStream;
+    /// The trait that this `Traversable` represents, parameterised by `interner`.
+    fn traversable(interner: &Interner<'_>) -> TokenStream;
 
-    /// The `match` arms for a traversal of this type.
-    fn arms(structure: &mut synstructure::Structure<'_>) -> TokenStream;
+    /// Any supertraits that this trait is required to implement.
+    fn supertraits(interner: &Interner<'_>) -> TokenStream;
 
-    /// The body of an implementation given the match `arms`.
-    fn impl_body(arms: impl ToTokens) -> TokenStream;
+    /// A (`noop`) traversal of this trait upon the `bind` expression.
+    fn traverse(bind: TokenStream, noop: bool) -> TokenStream;
+
+    /// A `match` arm for `variant`, where `f` generates the tokens for each binding.
+    fn arm(
+        variant: &synstructure::VariantInfo<'_>,
+        f: impl FnMut(&synstructure::BindingInfo<'_>) -> TokenStream,
+    ) -> TokenStream;
+
+    /// The body of an implementation given the `interner`, `traverser` and match expression `body`.
+    fn impl_body(
+        interner: Interner<'_>,
+        traverser: impl ToTokens,
+        body: impl ToTokens,
+    ) -> TokenStream;
 }
 
 impl Traversable for Foldable {
-    fn traversable() -> TokenStream {
-        quote! { ::rustc_middle::ty::fold::TypeFoldable<::rustc_middle::ty::TyCtxt<'tcx>> }
+    fn traversable(interner: &Interner<'_>) -> TokenStream {
+        quote! { ::rustc_middle::ty::fold::TypeFoldable<#interner> }
     }
-    fn arms(structure: &mut synstructure::Structure<'_>) -> TokenStream {
-        structure.each_variant(|vi| {
-            let bindings = vi.bindings();
-            vi.construct(|_, index| {
-                let bind = &bindings[index];
-
-                let mut fixed = false;
-
-                // retain value of fields with #[type_foldable(identity)]
-                bind.ast().attrs.iter().for_each(|x| {
-                    if !x.path().is_ident("type_foldable") {
-                        return;
-                    }
-                    let _ = x.parse_nested_meta(|nested| {
-                        if nested.path.is_ident("identity") {
-                            fixed = true;
-                        }
-                        Ok(())
-                    });
-                });
-
-                if fixed {
-                    bind.to_token_stream()
-                } else {
-                    quote! {
-                        ::rustc_middle::ty::fold::TypeFoldable::try_fold_with(#bind, __folder)?
-                    }
-                }
-            })
-        })
+    fn supertraits(interner: &Interner<'_>) -> TokenStream {
+        Visitable::traversable(interner)
     }
-    fn impl_body(arms: impl ToTokens) -> TokenStream {
+    fn traverse(bind: TokenStream, noop: bool) -> TokenStream {
+        if noop {
+            bind
+        } else {
+            quote! { ::rustc_middle::ty::fold::TypeFoldable::try_fold_with(#bind, folder)? }
+        }
+    }
+    fn arm(
+        variant: &synstructure::VariantInfo<'_>,
+        mut f: impl FnMut(&synstructure::BindingInfo<'_>) -> TokenStream,
+    ) -> TokenStream {
+        let bindings = variant.bindings();
+        variant.construct(|_, index| f(&bindings[index]))
+    }
+    fn impl_body(
+        interner: Interner<'_>,
+        traverser: impl ToTokens,
+        body: impl ToTokens,
+    ) -> TokenStream {
         quote! {
-            fn try_fold_with<__F: ::rustc_middle::ty::fold::FallibleTypeFolder<::rustc_middle::ty::TyCtxt<'tcx>>>(
+            fn try_fold_with<#traverser: ::rustc_middle::ty::fold::FallibleTypeFolder<#interner>>(
                 self,
-                __folder: &mut __F
-            ) -> ::core::result::Result<Self, __F::Error> {
-                ::core::result::Result::Ok(match self { #arms })
+                folder: &mut #traverser
+            ) -> ::core::result::Result<Self, #traverser::Error> {
+                ::core::result::Result::Ok(#body)
             }
         }
     }
 }
 
 impl Traversable for Visitable {
-    fn traversable() -> TokenStream {
-        quote! { ::rustc_middle::ty::visit::TypeVisitable<::rustc_middle::ty::TyCtxt<'tcx>> }
+    fn traversable(interner: &Interner<'_>) -> TokenStream {
+        quote! { ::rustc_middle::ty::visit::TypeVisitable<#interner> }
     }
-    fn arms(structure: &mut synstructure::Structure<'_>) -> TokenStream {
-        // ignore fields with #[type_visitable(ignore)]
-        structure.filter(|bi| {
-            let mut ignored = false;
-
-            bi.ast().attrs.iter().for_each(|attr| {
-                if !attr.path().is_ident("type_visitable") {
-                    return;
-                }
-                let _ = attr.parse_nested_meta(|nested| {
-                    if nested.path.is_ident("ignore") {
-                        ignored = true;
-                    }
-                    Ok(())
-                });
-            });
-
-            !ignored
-        });
-
-        structure.each(|bind| {
-            quote! {
-                ::rustc_middle::ty::visit::TypeVisitable::visit_with(#bind, __visitor)?;
-            }
-        })
+    fn supertraits(_: &Interner<'_>) -> TokenStream {
+        quote! { ::core::clone::Clone + ::core::fmt::Debug }
     }
-    fn impl_body(arms: impl ToTokens) -> TokenStream {
+    fn traverse(bind: TokenStream, noop: bool) -> TokenStream {
+        if noop {
+            quote! {}
+        } else {
+            quote! { ::rustc_middle::ty::visit::TypeVisitable::visit_with(#bind, visitor)?; }
+        }
+    }
+    fn arm(
+        variant: &synstructure::VariantInfo<'_>,
+        f: impl FnMut(&synstructure::BindingInfo<'_>) -> TokenStream,
+    ) -> TokenStream {
+        variant.bindings().iter().map(f).collect()
+    }
+    fn impl_body(
+        interner: Interner<'_>,
+        traverser: impl ToTokens,
+        body: impl ToTokens,
+    ) -> TokenStream {
         quote! {
-            fn visit_with<__V: ::rustc_middle::ty::visit::TypeVisitor<::rustc_middle::ty::TyCtxt<'tcx>>>(
+            fn visit_with<#traverser: ::rustc_middle::ty::visit::TypeVisitor<#interner>>(
                 &self,
-                __visitor: &mut __V
-            ) -> ::std::ops::ControlFlow<__V::BreakTy> {
-                match self { #arms }
-                ::std::ops::ControlFlow::Continue(())
+                visitor: &mut #traverser
+            ) -> ::core::ops::ControlFlow<#traverser::BreakTy> {
+                #body
+                ::core::ops::ControlFlow::Continue(())
             }
         }
     }
@@ -110,17 +229,50 @@ impl Traversable for Visitable {
 pub fn traversable_derive<T: Traversable>(
     mut structure: synstructure::Structure<'_>,
 ) -> TokenStream {
-    if let syn::Data::Union(_) = structure.ast().data {
-        panic!("cannot derive on union")
-    }
+    let ast = structure.ast();
 
-    structure.add_bounds(synstructure::AddBounds::Generics);
+    let interner = Interner::resolve(&ast.generics);
+    let traverser = gen_param("T", &ast.generics);
+    let traversable = T::traversable(&interner);
+
+    structure.underscore_const(true);
+    structure.add_bounds(synstructure::AddBounds::None);
     structure.bind_with(|_| synstructure::BindStyle::Move);
 
-    if !structure.ast().generics.lifetimes().any(|lt| lt.lifetime.ident == "tcx") {
+    if interner.lifetimes.is_empty() {
         structure.add_impl_generic(parse_quote! { 'tcx });
     }
 
-    let arms = T::arms(&mut structure);
-    structure.bound_impl(T::traversable(), T::impl_body(arms))
+    // If our derived implementation will be generic over the traversable type, then we must
+    // constrain it to only those generic combinations that satisfy the traversable trait's
+    // supertraits.
+    if ast.generics.type_params().next().is_some() {
+        let supertraits = T::supertraits(&interner);
+        structure.add_where_predicate(parse_quote! { Self: #supertraits });
+    }
+
+    // We add predicates to each generic field type, rather than to our generic type parameters.
+    // This results in a "perfect derive" that avoids having to propagate `#[skip_traversal]` annotations
+    // into wrapping types, but it can result in trait solver cycles if any type parameters are involved
+    // in recursive type definitions; fortunately that is not the case (yet).
+    let mut predicates = HashSet::new();
+    let arms = structure.each_variant(|variant| {
+        T::arm(variant, |bind| {
+            let ast = bind.ast();
+            let field_ty = interner.type_of(&bind.referenced_ty_params(), [ast]);
+            // we only need to add traversable predicate for generic types
+            if field_ty == Generic {
+                predicates.insert(ast.ty.clone());
+            }
+            T::traverse(bind.into_token_stream(), field_ty == Trivial)
+        })
+    });
+    // the order in which `where` predicates appear in rust source is irrelevant
+    #[allow(rustc::potential_query_instability)]
+    for ty in predicates {
+        structure.add_where_predicate(parse_quote! { #ty: #traversable });
+    }
+    let body = quote! { match self { #arms } };
+
+    structure.bound_impl(traversable, T::impl_body(interner, traverser, body))
 }
---
+++ compiler/rustc_macros/src/traversable/tests.rs
@@ -0,0 +1,155 @@
+use super::{parse_quote, traversable_derive, Foldable, ToTokens};
+use syn::visit_mut::VisitMut;
+
+/// A folder that normalizes syn types for comparison in tests.
+struct Normalizer;
+
+/// Generates a folding method for [`Normalizer`] that ensures certain collections
+/// are sorted consistently, thus eliminating any non-deterministic output.
+macro_rules! normalizing_sort {
+    ($($method:ident($field:ident in $ty:ty);)*) => {$(
+        fn $method(&mut self, i: &mut $ty) {
+            syn::visit_mut::$method(self, i);
+            let mut vec = std::mem::take(&mut i.$field).into_iter().collect::<Vec<_>>();
+            vec.sort_unstable_by_key(|x| x.to_token_stream().to_string());
+            i.$field = vec.into_iter().collect();
+        }
+    )*};
+}
+
+impl VisitMut for Normalizer {
+    // Each of the following fields in the following types can be reordered without
+    // affecting semantics, and therefore need to be normalized.
+    normalizing_sort! {
+        visit_where_clause_mut(predicates in syn::WhereClause);
+        visit_predicate_lifetime_mut(bounds in syn::PredicateLifetime);
+        visit_bound_lifetimes_mut(lifetimes in syn::BoundLifetimes);
+        visit_lifetime_param_mut(bounds in syn::LifetimeParam);
+        visit_type_param_mut(bounds in syn::TypeParam);
+        visit_type_impl_trait_mut(bounds in syn::TypeImplTrait);
+        visit_type_trait_object_mut(bounds in syn::TypeTraitObject);
+        visit_generics_mut(params in syn::Generics);
+    }
+
+    // For convenience, we also simplify paths by removing absolute crate/module
+    // references.
+    fn visit_path_mut(&mut self, i: &mut syn::Path) {
+        syn::visit_mut::visit_path_mut(self, i);
+
+        let n = if i.leading_colon.is_some() && i.segments.len() >= 2 {
+            let segment = &i.segments[0];
+            if *segment == parse_quote! { rustc_middle } {
+                if i.segments.len() >= 3 && i.segments[1] == parse_quote! { ty } {
+                    let segment = &i.segments[2];
+                    if *segment == parse_quote! { fold } || *segment == parse_quote! { visit } {
+                        3
+                    } else {
+                        2
+                    }
+                } else {
+                    1
+                }
+            } else if *segment == parse_quote! { core } {
+                let segment = &i.segments[1];
+                if *segment == parse_quote! { ops } {
+                    2
+                } else if *segment == parse_quote! { result } {
+                    i.segments.len() - 1
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
+        } else {
+            return;
+        };
+
+        *i = syn::Path {
+            leading_colon: None,
+            segments: std::mem::take(&mut i.segments).into_iter().skip(n).collect(),
+        };
+    }
+}
+
+#[test]
+fn only_potentially_non_trivial_fields_are_constrained_and_folded() {
+    let input = parse_quote! {
+        struct SomethingInteresting<'a, 'b, 'c, 'tcx: 'b, T>(
+            T,
+            T::Assoc,
+            Const<'tcx>,
+            Complex<'tcx, T>,
+            Generic<T>,
+            Trivial,
+            TrivialGeneric<'a, Foo>,
+            NotTrivial<'b>,
+            NotTrivial<'c>,
+        ) where 'tcx: 'c;
+    };
+
+    let expected = parse_quote! {
+        impl<'a, 'b, 'c, 'tcx: 'b, T> TypeFoldable<TyCtxt<'tcx>> for SomethingInteresting<'a, 'b, 'c, 'tcx, T>
+        where
+            'tcx: 'c,
+            Complex<'tcx, T>: TypeFoldable<TyCtxt<'tcx>>,
+            Generic<T>: TypeFoldable<TyCtxt<'tcx>>,
+            Self: TypeVisitable<TyCtxt<'tcx>>,
+            T: TypeFoldable<TyCtxt<'tcx>>,
+            T::Assoc: TypeFoldable<TyCtxt<'tcx>>
+            // the following constraints are NOT required, because the fields are not generic:
+            //     Const<'tcx>: TypeFoldable<TyCtxt<'tcx>>
+            //     Trivial: TypeFoldable<TyCtxt<'tcx>>
+            //     TrivialGeneric<'a, Foo>: TypeFoldable<TyCtxt<'tcx>>
+            //     NotTrivial<'b>: TypeFoldable<TyCtxt<'tcx>>
+            //     NotTrivial<'c>: TypeFoldable<TyCtxt<'tcx>>
+        {
+            fn try_fold_with<_T: FallibleTypeFolder<TyCtxt<'tcx>>>(self, folder: &mut _T) -> Result<Self, _T::Error> {
+                Ok(match self {
+                    SomethingInteresting (
+                        __binding_0,
+                        __binding_1,
+                        __binding_2,
+                        __binding_3,
+                        __binding_4,
+                        __binding_5,
+                        __binding_6,
+                        __binding_7,
+                        __binding_8,
+                    ) => { SomethingInteresting(
+                        TypeFoldable::try_fold_with(__binding_0, folder)?,
+                        TypeFoldable::try_fold_with(__binding_1, folder)?,
+                        TypeFoldable::try_fold_with(__binding_2, folder)?,
+                        TypeFoldable::try_fold_with(__binding_3, folder)?,
+                        TypeFoldable::try_fold_with(__binding_4, folder)?,
+                        __binding_5, // not folded
+                        __binding_6, // not folded
+                        TypeFoldable::try_fold_with(__binding_7, folder)?,
+                        TypeFoldable::try_fold_with(__binding_8, folder)?,
+                    )}
+                })
+            }
+        }
+    };
+
+    let result = syn::parse2::<syn::ItemConst>(traversable_derive::<Foldable>(
+        synstructure::Structure::new(&input),
+    ))
+    .expect("expected compiled code to parse");
+    let syn::Expr::Block(syn::ExprBlock { block: syn::Block { stmts, .. }, .. }) = *result.expr
+    else {
+        panic!("expected const expr to be a block")
+    };
+    assert_eq!(stmts.len(), 1, "expected const expr to contain a single statement");
+    let syn::Stmt::Item(syn::Item::Impl(mut actual)) = stmts.into_iter().next().unwrap() else {
+        panic!("expected statement in const expr to be an impl")
+    };
+    Normalizer.visit_item_impl_mut(&mut actual);
+
+    assert!(
+        actual == expected,
+        "EXPECTED: {}\nACTUAL:   {}",
+        expected.to_token_stream(),
+        actual.to_token_stream()
+    );
+}
